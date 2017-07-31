bf0c649569835b1c53ad2c29093303c9ed7293d3
C0smicThund3r <30605458+C0smicThund3r@users.noreply.github.com>
2017-07-31 22:32:33 +0100

    New javascript stuffs
    Looking at:
    Objects (unordered labled key values)
    Array (ordered values)
    Function (both old and new [arrow functions]}
    Array Prototypes (find filter)
    Strings (e.g. Surrey, Bin, York)
    Keys (e.g. Home, Age, Surname)
    Console log

--- index.js
+++ index.js
@@ -17,3 +17,50 @@ const numbers = [
 ]
 
 console.log(numbers)
+
+const Scott = {
+    forename: "Scott",
+    surname: "Greenhalgh",
+    age: 19,
+    home: "York"
+}
+
+console.log(Scott, Scott.forename)
+
+const Callum = {
+    forename: "Callum",
+    surname: "Denby",
+    age: 19,
+    home: "Surrey"
+}
+
+const People = [
+    Scott,
+    Callum
+]
+
+
+People.find(obj => obj.forename === "Callum")
+
+People.push({
+    forename: "Callum",
+    surname: "Wigglesworth",
+    age: 18,
+    home: "Bin"
+})
+
+console.log(People)
+
+const Callums = People.filter(person => person.forename === "Callum")
+
+console.log(Callums, People)
+
+function square(a) {
+    return a * a
+}
+
+console.log(square(10))
+
+const cube = a => a * a * a
+
+cube(10)
