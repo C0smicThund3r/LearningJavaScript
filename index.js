5e7e36c554541c1ac767010cc2c707117a48ac2b
C0smicThund3r <30605458+C0smicThund3r@users.noreply.github.com>
2017-07-31 21:44:14 +0100

    Our first javascript

---
+++ index.js
@@ -0,0 +1,18 @@
+// Single Line Comment
+/*
+    Multi Line Comment
+*/
+
+const forename = "Scott"
+const surname = "Greenhalgh"
+
+const fullname = forname + " " + surname
+
+console.log(fullname)
+const numbers = [
+    3,
+    7,
+    11
+]
+
+console.log(numbers)
